30be17812862bbdce3cb5ea8e65d21519a542345
Liubomur Puliak <liubomurpuliak@gmail.com>
2025-04-14 21:19:39 +0300

    add files

--- task08/outputs.tf
+++ task08/outputs.tf
@@ -5,5 +5,5 @@ output "aci_fqdn" {
 
 output "aks_lb_ip" {
   description = "Load Balancer IP address of APP in AKS"
-  value       = data.kubernetes_service.app_service.status.0.load_balancer.0.ingress.0.ip
+  value       = can(data.kubernetes_service.app_service.status.0.load_balancer.0.ingress.0.ip) ? data.kubernetes_service.app_service.status.0.load_balancer.0.ingress.0.ip : "Load Balancer IP not yet assigned"
 }
