5bb791a5782e74577959581787debd7ab3887b2d
Liubomur Puliak <liubomurpuliak@gmail.com>
2025-04-23 22:50:01 +0300

    fix name only for 3 days

--- task08/locals.tf
+++ task08/locals.tf
@@ -2,7 +2,8 @@ locals {
   rg_name                     = var.resource_group_name
   redis_name                  = "${var.name_pattern}-redis"
   keyvault_name               = "${var.name_pattern}-kv"
-  acr_name                    = substr(lower(replace("${var.name_pattern}cr", "-", "")), 0, 50)
+  acr_name = "${var.name_pattern}cr-temp"
+ # acr_name                    = substr(lower(replace("${var.name_pattern}cr", "-", "")), 0, 50)
   aks_name                    = "${var.name_pattern}-aks"
   aci_name                    = "${var.name_pattern}-ci"
   image_tag                   = "latest"
