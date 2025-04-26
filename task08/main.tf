38a9fca140f565a279e48e449fbb5acf8ea73be3
Liubomur Puliak <liubomurpuliak@gmail.com>
2025-04-26 23:07:23 +0300

    try 99990

--- task08/main.tf
+++ task08/main.tf
@@ -1,18 +1,5 @@
 # main.tf
 
-locals {
-  rg_name                     = var.resource_group_name
-  redis_name                  = "${var.name_pattern}-redis"
-  keyvault_name               = "${var.name_pattern}-kv"
-  acr_name                    = substr(lower(replace("${var.name_pattern}cr", "-", "")), 0, 50)
-  aks_name                    = "${var.name_pattern}-aks"
-  aci_name                    = "${var.name_pattern}-ci"
-  image_tag                   = "latest"
-  build_context_relative_path = "task08/application"
-  # Додаємо ім'я для нової UAMI
-  aks_kv_identity_name = "${local.aks_name}-kv-identity"
-}
-
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "rg" {
@@ -217,16 +204,4 @@ data "kubernetes_service" "app_service" {
     namespace = "default" # Припускаємо default namespace
   }
   depends_on = [kubectl_manifest.service] # Залежимо від Service
-}
-
-
-output "aci_fqdn" {
-  description = "FQDN of App in Azure Container Instance"
-  value       = module.aci.aci_fqdn
-}
-
-output "aks_lb_ip" {
-  description = "Load Balancer IP address of APP in AKS"
-  # Використовуємо can() для безпечного доступу, якщо LoadBalancer IP ще не призначено
-  value       = can(data.kubernetes_service.app_service.status.0.load_balancer.0.ingress.0.ip) ? data.kubernetes_service.app_service.status.0.load_balancer.0.ingress.0.ip : "Load Balancer IP not yet assigned or service not ready"
 }
